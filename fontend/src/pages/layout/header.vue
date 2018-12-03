fbb8eecf45aec7dead2e564f4cfa70a2f797552f
TOKdawn <dawninshadow0.0@gmail.com>
2018-12-03 17:47:27 +0800

    update

--- fontend/src/pages/layout/header.vue
+++ fontend/src/pages/layout/header.vue
@@ -6,13 +6,8 @@
       mode="horizontal"
       @select="handleSelect"
     >
-      <el-menu-item index="1">主页</el-menu-item>
-      <el-submenu index="2">
-        <template slot="title">谱册</template>
-        <el-menu-item index="2-1">选项1</el-menu-item>
-        <el-menu-item index="2-2">选项2</el-menu-item>
-        <el-menu-item index="2-3">选项3</el-menu-item>
-      </el-submenu>
+      <el-menu-item index="1" @click="$router.push({ path: `/index` })">主页</el-menu-item>
+      <el-menu-item index="2" @click="$router.push({ path: `/page/scorelist` })">谱册</el-menu-item>
       <el-menu-item
         index="3"
         disabled
