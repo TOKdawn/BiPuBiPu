69896a6092dfded93c55e10d66375c2fd96a9918
TOKdawn <dawninshadow0.0@gmail.com>
2018-12-14 15:44:03 +0800

    background init

---
+++ background/config/config.prod.js
@@ -0,0 +1,16 @@
+'use strict'
+exports.cluster = {
+    listen: {
+        port: 7001,
+        hostname: '0.0.0.0',
+        // path: '/var/run/egg.sock',
+    },
+    // static: {
+    //     prefix: '/',
+    //     dir: path.join(appInfo.baseDir, 'app/public'),
+    //     dynamic: true,
+    //     preload: false,
+    //     buffer: false,
+    //     maxFiles: 100000
+    // }
+}
---
+++ background/config/plugin.js
@@ -0,0 +1,34 @@
+'use strict';
+exports.sequelize = {
+    enable: true,
+    package: 'egg-sequelize',
+};
+exports.oss = {
+    enable: true,
+    package: 'egg-oss',
+};
+module.exports.passport = {
+    enable: true,
+    package: 'egg-passport',
+};
+
+module.exports.passportGithub = {
+    enable: true,
+    package: 'egg-passport-github',
+};
+exports.cors = {
+    enable: true,
+    package: 'egg-cors',
+};
+exports.static = {
+        enable: true,
+        package: 'egg-static',
+};
+
+    // module.exports.passportWeibo = {
+    //     enable: true,
+    //     package: 'egg-passport-weibo',
+    // };
+
+// had enabled by egg
+// exports.static = true;
