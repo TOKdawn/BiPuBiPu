aad6502b4729bced379d2fdfa1d41cc077d9697b
TOKdawn <dawninshadow0.0@gmail.com>
2019-01-02 16:38:10 +0800

    随便更新下

--- background/app/controller/volume.js
+++ background/app/controller/volume.js
@@ -7,146 +7,154 @@ const DEFAULTSCOREPAGESIZE = 20;
 
 class VolumeController extends Controller {
 
-    constructor(ctx) {
-        super(ctx);
-        this.VolumeService = ctx.service.volumeService;
-        // this.ctx.session.uid = 123; // 测试用
+  constructor(ctx) {
+    super(ctx);
+    this.VolumeService = ctx.service.volumeService;
+    // this.ctx.session.uid = 123; // 测试用
+  }
+  async getVolumeList() {
+    const {
+      offset = DEFAULTOFFSET, pagesize = DEFAULTVOLUMEPAGESIZE, role = 'normal'
+    } = this.ctx.query;
+    const response = await this.VolumeService.getVolumeList(offset, pagesize, role);
+    this.ctx.helper.successRes('sucess', response);
+  }
+
+  async createVolume() {
+    const {
+      title,
+      describe
+    } = this.ctx.request.body;
+    const uid = this.ctx.user.id;
+    // const uid = 123; //测试用
+    const response = await this.VolumeService.createVolume(title, describe, uid);
+    this.ctx.helper.successRes('sucess', response);
+  }
+
+  async editVolume() {
+    const {
+      title,
+      describe,
+      url
+    } = this.ctx.request.body;
+    const {
+      vid
+    } = this.ctx.params;
+    const volume = await this.VolumeService.findOwner(vid);
+    if (volume.get(uid) !== this.ctx.user.id) {
+      this.ctx.helper.createRes(403, 'permission denied ಠ益ಠ');
     }
-    async getVolumeList() {
+    const response = await this.VolumeService.editVolume(vid, title, describe, url);
+    this.ctx.helper.successRes('sucess', response);
+  }
 
-        const { offset = DEFAULTOFFSET, pagesize = DEFAULTVOLUMEPAGESIZE, role = 'normal' } = this.ctx.query;
-        const response = await this.VolumeService.getVolumeList(offset, pagesize, role);
-        this.ctx.body = response;
-    }
+  async deleteVolume() {
+    const {
+      vid
+    } = this.ctx.params;
+    const uid = this.ctx.user.id;
+    const response = await this.VolumeService.deleteVolume(vid, uid);
+    if (response) {
+      this.ctx.helper.createRes(200, 'Delete success QwQ');
 
-    async createVolume() {
-        const {
-            title,
-            describe,
-        } = this.ctx.request.body;
-        const uid = this.ctx.user.id;
-        // const uid = 123; //测试用
-        const response = await this.VolumeService.createVolume(title, describe, uid);
-        this.ctx.body = response;
-    }
-
-    async editVolume() {
-        const {
-            title,
-            describe,
-            url,
-
-        } = this.ctx.request.body;
-        const { vid } = this.ctx.params;
-        const volume = await this.VolumeService.findOwner(vid);
-        if (volume.get(uid) !== this.ctx.user.id) {
-            this.ctx.helper.createRes(403, 'permission denied ಠ益ಠ');
-        }
-        const response = await this.VolumeService.editVolume(vid, title, describe, url);
-        this.ctx.body = response;
-    }
-
-    async deleteVolume() {
-        const { vid } = this.ctx.params;
-        const uid = this.ctx.user.id;
-
-        const response = await this.VolumeService.deleteVolume(vid, uid);
-        if (response) {
-            this.ctx.helper.createRes(200, 'Delete success QwQ');
-
-        } else {
-            this.ctx.helper.createRes(409, 'Delete err Orz  ');
-
-        }
-        this.ctx.body = response;
-    }
-
-    async getVolumeInfo() {
-        const { vid } = this.ctx.params;
-        const response = await this.VolumeService.getVolumeInfo(vid);
-        this.ctx.body = response;
-    }
-
-    async getVolumeScore() {
-        const { vid } = this.ctx.params;
-        const { offset = DEFAULTOFFSET, pagesize = DEFAULTSCOREPAGESIZE } = this.ctx.query;
-
-        const response = await this.VolumeService.getVolumeScore(vid, offset, pagesize);
-        // console.log(response)
-        this.ctx.body = response;
-
-
+    } else {
+      this.ctx.helper.createRes(409, 'Delete err Orz  ');
 
     }
+    this.ctx.helper.successRes('sucess', response);
+  }
 
-    async addVolumeScore() {
-        const {
-            sid,
-        } = this.ctx.request.body;
-        const {
-            vid,
-        } = this.ctx.params;
-        const volume = await this.VolumeService.findOwner(vid);
+  async getVolumeInfo() {
+    const {
+      vid
+    } = this.ctx.params;
+    const response = await this.VolumeService.getVolumeInfo(vid);
+    this.ctx.helper.successRes('sucess', response);
+  }
 
-        if (volume.get('uid') !== this.ctx.user.id) {
-            this.ctx.helper.createRes(403, 'permission denied ಠ益ಠ');
-        }
-        const response = await this.VolumeService.addVolumeScore(vid, sid);
-        this.ctx.body = response;
+  async getVolumeScore() {
+    const {
+      vid
+    } = this.ctx.params;
+    const {
+      offset = DEFAULTOFFSET, pagesize = DEFAULTSCOREPAGESIZE
+    } = this.ctx.query;
+    const response = await this.VolumeService.getVolumeScore(vid, offset, pagesize);
+    // console.log(response)
+    this.ctx.helper.successRes('sucess', response);
+  }
+
+  async addVolumeScore() {
+    const {
+      sid
+    } = this.ctx.request.body;
+    const {
+      vid
+    } = this.ctx.params;
+    const volume = await this.VolumeService.findOwner(vid);
+
+    if (volume.get('uid') !== this.ctx.user.id) {
+      this.ctx.helper.createRes(403, 'permission denied ಠ益ಠ');
     }
+    const response = await this.VolumeService.addVolumeScore(vid, sid);
+    this.ctx.helper.successRes('sucess', response);
+  }
 
-    async deleteVolumeScore() {
-        const { vid, sid } = this.ctx.params;
-        const volume = await this.VolumeService.findOwner(vid);
+  async deleteVolumeScore() {
+    const {
+      vid,
+      sid
+    } = this.ctx.params;
+    const volume = await this.VolumeService.findOwner(vid);
+    if (volume.get('uid') !== this.ctx.user.id) {
+      this.ctx.helper.createRes(403, 'permission denied ಠ益ಠ');
+    }
+    const response = await this.VolumeService.deleteVolumeScore(vid, sid);
+    this.ctx.helper.successRes('sucess', response);
+  }
 
-        if (volume.get('uid') !== this.ctx.user.id) {
-            this.ctx.helper.createRes(403, 'permission denied ಠ益ಠ');
-        }
-        const response = await this.VolumeService.deleteVolumeScore(vid, sid);
-        this.ctx.body = response;
-    }
-    async getComment() {
-        const {
-            vid,
-        } = this.ctx.params;
-        const response = await this.VolumeService.getComment(vid);
-        this.ctx.body = response;
-    }
-    async addComment() {
-        const {
-            vid,
-        } = this.ctx.params;
+  async getComment() {
+    const {
+      vid
+    } = this.ctx.params;
+    const response = await this.VolumeService.getComment(vid);
+    this.ctx.helper.successRes('sucess', response);
+  }
 
-        const {
-            replyid,
-            text,
-            targetid
-        } = this.ctx.request.body;
-        const uid = this.ctx.user.id;
+  async addComment() {
+    const {
+      vid
+    } = this.ctx.params;
+    const {
+      replyid,
+      text,
+      targetid
+    } = this.ctx.request.body;
+    const uid = this.ctx.user.id;
+    let response;
+    if (replyid) {
+      response = await this.VolumeService.addCommentToComment(replyid, text, uid, targetid);
+    } else {
+      response = await this.VolumeService.addCommentToVolume(vid, text, uid);
+    }
+    this.ctx.helper.successRes('sucess', response);
+  }
 
-        let response;
-        if (replyid) {
-            response = await this.VolumeService.addCommentToComment(replyid, text, uid, targetid);
-        } else {
-            response = await this.VolumeService.addCommentToVolume(vid, text, uid);
-        }
-
-        this.ctx.body = response;
-    }
-    async delectComment() {
-        const {
-            cid,
-        } = this.ctx.params;
-        const response = await this.VolumeService.delectComment(cid);
-        this.ctx.body = response;
-    }
-    async delectSubcomment() {
-        const {
-            cid,
-        } = this.ctx.params;
-        const response = await this.VolumeService.delectSubcomment(cid);
-        this.ctx.body = response;
-    }
+  async delectComment() {
+    const {
+      cid
+    } = this.ctx.params;
+    const response = await this.VolumeService.delectComment(cid);
+    this.ctx.helper.successRes('sucess', response);
+  }
+  
+  async delectSubcomment() {
+    const {
+      cid
+    } = this.ctx.params;
+    const response = await this.VolumeService.delectSubcomment(cid);
+    this.ctx.helper.successRes('sucess', response);
+  }
 }
 
 module.exports = VolumeController;
--- background/app/model/score.js
+++ background/app/model/score.js
@@ -13,17 +13,52 @@ module.exports = app => {
       primaryKey: true,
       autoIncrement: true,
     },
-    name: {
+    name: { //标题
       type: STRING,
       allowNull: false,
-
     },
-    sid: {
+    alias:{ //别名
+      type: STRING,
+      allowNull: true,
+    },
+    addtion:{ //简介
+      type: STRING,
+      allowNull: true,
+    },
+    image_url: { //图片
+      type: STRING,
+      allowNull: false,
+    },
+    score_text:{//内容本体
+      type: STRING,
+      allowNull: false,
+    },
+    source_url:{//原谱地址
+      type:STRING,
+      allowNull:true,
+    },
+    provider:{//扒谱人
+      type:STRING,
+      allowNull:true,
+    },
+    provider_url:{ //扒谱人url
+      type:STRING,
+      allowNull:true,
+    },
+    carrier:{ //传谱人
+      type:STRING,
+      allowNull:true,
+    },
+    performs:{//B站作品
       type: INTEGER,
       allowNull: false,
     },
+    songs:{ //网易云音乐
+      type: INTEGER,
+      allowNull: false,
+    }
   }, {
-    timestamps: false,
+    timestamps: true,
     freezeTableName: true, // 默认表名会被加s,此选项强制表名跟model一致
   });
 
